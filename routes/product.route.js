2a56f759186decce818a5aeda9560d57f12ab5da
Hafsa Khan Lodhi <hafsalodhi2023@gmail.com>
2025-04-29 19:42:49 +0500

    Fix: Apply isAdmin middleware to product creation route
    This commit applies the `isAdmin` middleware to the `/api/products/create` route to ensure that only administrators can create new products.
    
    - Adds `isAdmin` middleware to the product creation route to restrict access to administrators.

--- routes/product.route.js
+++ routes/product.route.js
@@ -4,12 +4,13 @@ const create = require("../controllers/products/create.product.controller"); // 
 
 // Import middlewares
 const auth = require("../middlewares/auth.middleware"); // Import auth middleware
+const isAdmin = require("../middlewares/isadmin.middleware"); // Import isAdmin middleware
 
 const router = express.Router(); // Create router
 
 // @route POST /api/products/create
 // @desc Create new product
 // @access Private/Admin
-router.post("/create", auth, create);
+router.post("/create", auth, isAdmin, create);
 
 module.exports = router; // Export router
