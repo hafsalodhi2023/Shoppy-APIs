84a7fdec7d418513a254eaa262cc40d23b5cb5cd
Hafsa Khan Lodhi <hafsalodhi2023@gmail.com>
2025-05-05 15:31:57 +0500

    Feat: Initialize dotenv and mongoose connection in seeder file

--- seeder.js
+++ seeder.js
@@ -1,3 +1,12 @@
 const Product = require("./models/Product");
 const User = require("./models/User");
 const products = require("./data/products");
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URI);
+
+const seedData = async () => {
+  try {
+  } catch (error) {}
+};
