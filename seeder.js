e531118b0ebb9ba92f853b584577bc709e524888
Hafsa Khan Lodhi <hafsalodhi2023@gmail.com>
2025-05-06 13:24:53 +0500

    Feat: Implement data seeding functionality
    This commit implements the data seeding functionality to populate the database with initial data, including a default admin user and sample products.
    
    - Adds a `seed` script to `package.json` to run the seeder.
    - Implements the `seedData` function in `seeder.js` to:
      - Clear existing product and user data.
      - Create a default admin user.
      - Assign the admin user to sample products.
      - Insert the sample products into the database.
    - Adds `debug` logging for seeding process.
    - Implements error handling for the seeding process.

--- seeder.js
+++ seeder.js
@@ -1,5 +1,10 @@
-const Product = require("./models/Product");
-const User = require("./models/User");
+const debug = require("debug")("server:seeder.js");
+
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+const Product = require("./models/product.model");
+const User = require("./models/user.model");
 const products = require("./data/products");
 
 dotenv.config();
@@ -7,6 +12,34 @@ dotenv.config();
 mongoose.connect(process.env.MONGO_URI);
 
 const seedData = async () => {
+  debug("Seeding data...");
   try {
-  } catch (error) {}
+    // Clear Existing data
+    await Product.deleteMany();
+    await User.deleteMany({});
+
+    // Create a default admin user
+    const createdUser = await User.create({
+      name: "Admin",
+      email: "admin@example.com",
+      password: "123456789",
+      role: "admin",
+    });
+
+    // Assign the default admin user to each product
+    const userID = createdUser._id;
+
+    const sampleProducts = products.map((product) => {
+      return { ...product, user: userID };
+    });
+
+    await Product.insertMany(sampleProducts);
+    debug("Data seeded successfully!");
+    process.exit();
+  } catch (error) {
+    debug("Error seeding data:", error.message);
+    process.exit(1);
+  }
 };
+
+seedData();
