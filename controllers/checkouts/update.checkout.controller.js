1704093ba94874b18853eb37889a1e3b9078775e
Hafsa Khan Lodhi <hafsalodhi2023@gmail.com>
2025-06-04 14:40:46 +0500

    feat(checkout): add update checkout controller with payment handling
    Implement controller to handle checkout updates, including payment status validation and updating checkout details. The controller returns appropriate responses for success, not found, invalid status, and error cases.

--- controllers/checkouts/update.checkout.controller.js
+++ controllers/checkouts/update.checkout.controller.js
@@ -1,5 +1,59 @@
+// @collapse
 const debug = require("debug")(
   "server:controllers:checkouts:update.checkout.controller.js"
 );
 
 const Checkout = require("../../models/checkout.model"); // Import Checkout model
+
+const update = async (req, res) => {
+  const { paymentStatus, paymentDetails } = req.body;
+
+  try {
+    debug("Request PUT /api/checkout/:id");
+    const checkout = await Checkout.findById(req.params.id);
+
+    if (!checkout) {
+      debug("Request PUT /api/checkout/:id: Checkout not found");
+      return res.status(404).json({
+        success: false,
+        error: true,
+        data: null,
+        message: "Checkout not found",
+      });
+    }
+
+    if (paymentStatus === "paid") {
+      checkout.isPaid = true;
+      checkout.paymentStatus = paymentStatus;
+      checkout.paymentDetails = paymentDetails;
+      checkout.paidAt = Date.now();
+      await checkout.save();
+
+      debug("Request PUT /api/checkout/:id: Checkout updated successfully!");
+      return res.status(200).json({
+        success: true,
+        error: false,
+        data: checkout,
+        message: "Checkout updated successfully",
+      });
+    } else {
+      debug("Request PUT /api/checkout/:id: Invalid payment status");
+      return res.status(400).json({
+        success: false,
+        error: true,
+        data: null,
+        message: "Invalid payment status",
+      });
+    }
+  } catch (error) {
+    debug("Request PUT /api/checkout/:id: ", error);
+    return res.status(500).json({
+      success: false,
+      error: true,
+      data: null,
+      message: "Internal server error",
+    });
+  }
+};
+
+module.exports = update;
