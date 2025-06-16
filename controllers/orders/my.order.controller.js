814dce61441d4a421e26dcb9d1b7b206fad0c41b
Hafsa Khan Lodhi <hafsalodhi2023@gmail.com>
2025-06-16 15:28:43 +0500

    feat(orders): implement my orders endpoint to fetch user orders
    Add functionality to retrieve orders for authenticated users with error handling. Orders are sorted by creation date in descending order.

--- controllers/orders/my.order.controller.js
+++ controllers/orders/my.order.controller.js
@@ -4,6 +4,30 @@ const debug = require("debug")(
 
 const Order = require("../../models/order.model"); // Import Order model
 
-const myOrder = async (req, res) => {};
+const myOrder = async (req, res) => {
+  try {
+    debug("Request GET /api/orders/my-orders");
+
+    // Find orders for the authenticated user
+    const orders = await Order.find({ user: req.user._id }).sort({
+      createdAt: -1,
+    }); // Sort orders by creation date in descending order
+    debug("Request GET /api/orders/my-orders: Orders fetched successfully!");
+    return res.status(201).json({
+      success: true,
+      error: false,
+      data: orders,
+      message: "Orders fetched successfully",
+    });
+  } catch (error) {
+    debug("Request GET /api/orders/my-orders: Failed to fetch orders", error);
+    return res.status(500).json({
+      success: false,
+      error: true,
+      data: null,
+      message: "Internal Server Error.",
+    });
+  }
+};
 
 module.exports = myOrder;
